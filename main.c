d97e745587df32d3a60a74e4f46bf21f5c32fa59
tie1e <tielemunizalves@gmail.com>
2024-11-17 21:14:15 -0300

    Teste_1

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
+    int tamanho;
+    int *vetor;
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+
+    vetor = (int*)malloc(tamanho*sizeof(int));
+
+    printf("Digite os elementos do vetor:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("\nOs dados do vetor são:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("Elemento %d: %d\n", i + 1, vetor[i]);
+    }
+
+    free(vetor);
 
     return 0;
 }
